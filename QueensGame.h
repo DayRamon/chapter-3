7ff85b5df0548663e2699063a58efba014db43cd
Anthony1418 <anthonyherrera2314@gmail.com>
2025-09-15 17:31:14 -0700

    Update QueensGame.h

--- QueensGame.h
+++ QueensGame.h
@@ -14,10 +14,24 @@ private:
     bool isSafe(int row, int col) const;  // Check if a move is safe
 
 public:
+    //Precondition: none
+    //Postcondition: Constructor
     QueensGame(int size);
-    bool placeQueen(int row, int col); // tries to place, returns success/fail
+
+    //Precondition: Takes valid input for row and col from user
+    //Poscondition: Places queen or tells user if invalid
+    bool placeQueen(int row, int col); 
+
+    //Precondition: Takes user input for an existing queen 
+    //Postcondition: Removes queen or tells user if invalid
     void removeQueen(int row, int col);
-    bool allQueensPlaced() const;      // check if done
+
+    //Precondition: Checks if all queens have been placed
+    //Postcondition: Returns placed
+    bool allQueensPlaced() const;    
+
+    //Precondition: None
+    //Postcondition: Displays board
     void showBoard() const;
 };
 
